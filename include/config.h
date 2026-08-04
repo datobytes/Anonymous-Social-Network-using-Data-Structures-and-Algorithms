885269050ba394e17fd835e97f8a3d26bc1f35b2
datobytes <hoangdat09062006@gmail.com>
2026-08-04 20:11:25 +0700

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+#define MAX_NAME_LEN    50
+#define MAX_CONTENT_LEN 500
+#define ALPHABET_SIZE   256
+
+#endif // CONFIG_H
